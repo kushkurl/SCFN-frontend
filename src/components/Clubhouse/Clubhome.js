201ea3dffd62e3278750ed579e030285c6a0a624
kushkurl <kushagra.kurl@gmail.com>
2022-06-01 19:19:55 -0400

    updated readme file and added club house component

---
+++ src/components/Clubhouse/Clubhome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function clubhome(props) {
+
+
+  return (
+    <div>
+      <h1>This will be the home page after user is registered!</h1>
+    </div>
+  );
+}
+
+export default clubhome;
